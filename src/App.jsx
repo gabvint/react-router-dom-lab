fa89c2d90519c928d27440778fb72d0e03ecc455
gabtongol <geybtongol@gmail.com>
2024-08-16 15:33:04 -0400

    created files for components and installed RRD

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 // src/App.jsx
+import Navbar from './components/NavBar.jsx'; 
+import MailboxForm from './components/MailboxForm.jsx';
+import MailboxList from './components/MailboxList.jsx';
+import MailboxDetails from './components/MailboxDetails.jsx';
+
 
 const App = () => {
   return <h1>Hello world!</h1>;
